c6e5adb5a98f3ca0aa6293ff260da27f047f581f
leosquintero <leonardosquintero@gmail.com>
2019-05-14 18:16:02 -0400

    Update Data Cleaning and EDA.R
    added code for dealing with NAs and daleted columns  "Alley","PoolQC", "Fence",
                                                 "MiscFeature"

--- data cleaning/Data Cleaning and EDA.R
+++ data cleaning/Data Cleaning and EDA.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 library(tidyverse)
-
+library(dummies)
 
 #####Data Cleaning#####
 
@@ -9,7 +9,7 @@ library(tidyverse)
 train = read.csv("./train.csv", stringsAsFactors = FALSE)
 test = read.csv("./test.csv", stringsAsFactors = FALSE)
 
-#### EDA ####
+#### wrangling and cleaning ####
 
 # Taking a look at the head
 head(train)
@@ -17,20 +17,49 @@ head(test)
 
 # taking a look at the dimentions
 dim(train)
-dim(test)
 
 #taking a look at the structure
 str(train)
-str(test)
 
 # taking a look at the summary
 summary(train)
-summary(test)
-
 
 colnames(train)
 
+# Droping column ID
+train <- train[, -1]
 
-# counting missing values per column and assigning to missing_val
-missing_val <- sapply(train, function(y) sum(length(which(is.na(y)))))
+
+#### Missing values #### 
+
+# Percentage of data missing in the data frame (0.05889565)
+sum(is.na(train)) / (nrow(train) *ncol(train))
+
+# counting missing values per column
+data.frame(sapply(train, function(y) sum(length(which(is.na(y))))))
+
+# replacing NA by the mean of each numerical column
+for(i in 1:ncol(train)){
+    train[is.na(train[,i]), i] <- mean(train[,i], na.rm = TRUE)
+}
+
+# Erasing columns with too many missing values
+train <- train[ , -which(names(train) %in% c("Alley","PoolQC", "Fence", 
+                                             "MiscFeature"))]
+
+
+
+# Select only columns that add value to a property and subset
+# dummify Categorical variables if necessary
+# drop rows with NAN or impute (case sensitive)
+#dummify
+dummy(train$Street, 'Pave')
+train$Street <- ifelse(train$Street == "Pave", 1, 0)
+
+#### EDA ####
+
+qplot(train$GrLivArea, train$SalePrice, main = "With Outliers")
+
+
+
 
