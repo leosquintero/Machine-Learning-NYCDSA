0ea3bc386040efab26bf7a0d811d5db0cafbe812
leosquintero <leonardosquintero@gmail.com>
2019-05-15 17:30:38 -0400

    Update Data Cleaning and EDA.R

--- data cleaning/Data Cleaning and EDA.R
+++ data cleaning/Data Cleaning and EDA.R
@@ -1,19 +1,22 @@
 library(ggplot2)
 library(tidyverse)
 library(dummies)
+library(fastDummies)
+library(class)
+library(corrplot)
+library(caret)
 
 #####Data Cleaning#####
 
 
 #uploading data sets
-train = read.csv("./train.csv", stringsAsFactors = FALSE)
-test = read.csv("./test.csv", stringsAsFactors = FALSE)
+train = read.csv("train.csv", stringsAsFactors = FALSE)
 
 #### wrangling and cleaning ####
 
 # Taking a look at the head
 head(train)
-head(test)
+
 
 # taking a look at the dimentions
 dim(train)
@@ -30,7 +33,7 @@ colnames(train)
 train <- train[, -1]
 
 
-#### Missing values #### 
+#### Missing Data #### 
 
 # Percentage of data missing in the data frame (0.05889565)
 sum(is.na(train)) / (nrow(train) *ncol(train))
@@ -38,27 +41,61 @@ sum(is.na(train)) / (nrow(train) *ncol(train))
 # counting missing values per column
 data.frame(sapply(train, function(y) sum(length(which(is.na(y))))))
 
-# replacing NA by the mean of each numerical column
+# Imputing missing data with the mean of each numerical column
 for(i in 1:ncol(train)){
     train[is.na(train[,i]), i] <- mean(train[,i], na.rm = TRUE)
 }
 
 # Erasing columns with too many missing values
 train <- train[ , -which(names(train) %in% c("Alley","PoolQC", "Fence", 
-                                             "MiscFeature"))]
+                                             "MiscFeature", "FireplaceQu"))]
+
+# Deleting rows with remaining missing values
+train <- train[complete.cases(train), ]
+
+# Counting missing values again
+sum(is.na(train)) / (nrow(train) *ncol(train))
 
 
+#### Dummies ####
 
-# Select only columns that add value to a property and subset
-# dummify Categorical variables if necessary
-# drop rows with NAN or impute (case sensitive)
-#dummify
-dummy(train$Street, 'Pave')
-train$Street <- ifelse(train$Street == "Pave", 1, 0)
+# dummifying Categorical(Factor) columns 
+t <- dummyVars("~.", data = train, drop2nd = TRUE)
+train <- data.frame(predict(t, newdata = train))
+
 
 #### EDA ####
 
+# evaluate relevant variables and plot with and without outliers
 qplot(train$GrLivArea, train$SalePrice, main = "With Outliers")
+
+# Deleting outliers
+train <- train[-which(train$GrLivArea > 4000),]
+ 
+#plot without outliers
+qplot(train$GrLivArea, train$SalePrice, main = "Without Outliers")
+
+
+
+# Selecting relevant variables to subset
+
+train.model<- lm(SalePrice ~ .,  data = train)
+summary(train.model)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
