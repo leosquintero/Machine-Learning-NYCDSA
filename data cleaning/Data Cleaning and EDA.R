8af9808d35f2c95c2df5430947d885cd935a9982
leosquintero <leonardosquintero@gmail.com>
2019-05-25 15:45:49 -0400

    Update Data Cleaning and EDA.R

--- data cleaning/Data Cleaning and EDA.R
+++ data cleaning/Data Cleaning and EDA.R
@@ -7,7 +7,7 @@ library(caret)
 library(broom)
 library(purrr)
 library(ggplot2)
-
+library(gridExtra)
 
 #####Data Cleaning#####
 
@@ -70,10 +70,6 @@ sum(is.na(train)) / (nrow(train) *ncol(train))
 # dummifying Categorical(Factor) columns
 t <- dummyVars("~.", data = train, drop2nd = TRUE)
 train <- data.frame(predict(t, newdata = train))
-
-
-
-
 
 
 
@@ -146,7 +142,7 @@ ggplot(train_rel, aes(x = SalePrice, fill = ..count..)) +
     geom_histogram(binwidth = 0.05) +
     ggtitle("Figure 2 Histogram of log SalePrice") +
     ylab("Count of houses") +
-    xlab("Housing Price") + 
+    xlab("Housing Price normalized") + 
     theme(plot.title = element_text(hjust = 0.5))
 
 # Plotting Sale price compared to lotArea
@@ -157,29 +153,43 @@ plot_1 <- ggplot(train_rel, aes(x= SalePrice, y = LotArea)) +
     theme(plot.title = element_text(hjust = 0.4))
 
 
-# Plotting Sale price compared to lotArea
+# Plotting Sale price compared to Year Built
 plot_2 <- ggplot(train_rel, aes(x= SalePrice, y = YearBuilt)) +
     geom_point()+
     geom_smooth(method=lm , color="blue", se=FALSE) +
-    ggtitle("Scatterplot of Sale Price vs Lot Area") +
+    ggtitle("Scatterplot of Sale Price vs Year Built") +
     theme(plot.title = element_text(hjust = 0.4))
 
 
-# Plotting Sale price compared to lotArea
+# Plotting Sale price compared to Wood Deck SF
 plot_3 <- ggplot(train_rel, aes(x= SalePrice, y = WoodDeckSF)) +
     geom_point()+
     geom_smooth(method=lm , color="blue", se=FALSE) +
-    ggtitle("Scatterplot of Sale Price vs Lot Area") +
+    ggtitle("Scatterplot of Sale Price vs Wood Deck SF") +
     theme(plot.title = element_text(hjust = 0.4))
 
+#  Plotting Sale price compared to Total Square feet
 plot_4 <- ggplot(train_rel, aes(x= SalePrice, y = TotalSF)) +
     geom_point()+
     geom_smooth(method=lm , color="blue", se=FALSE) +
-    ggtitle("Scatterplot of Sale Price vs Lot Area") +
+    ggtitle("Scatterplot of Sale Price vs Total Square feet") +
     theme(plot.title = element_text(hjust = 0.4))
 
-library(gridExtra)
-grid.arrange(plot_1, plot_2,plot_3,plot_4)
+#  Plotting Sale price compared to Above grade (ground) living area square fee
+plot_5 <- ggplot(train_rel, aes(x= SalePrice, y = GrLivArea)) +
+    geom_point()+
+    geom_smooth(method=lm , color="blue", se=FALSE) +
+    ggtitle("Scatterplot of Sale Price vs Above grade (ground) living area square feet") +
+    theme(plot.title = element_text(hjust = 0.4))
+
+#  Plotting Sale price compared to Masonry veneer area in square feet
+plot_6 <- ggplot(train_rel, aes(x= SalePrice, y = MasVnrArea)) +
+    geom_point()+
+    geom_smooth(method=lm , color="blue", se=FALSE) +
+    ggtitle("Scatterplot of Sale Price vs Masonry veneer area in square feet") +
+    theme(plot.title = element_text(hjust = 0.4))
+
+grid.arrange(plot_1, plot_2,plot_3,plot_4, plot_5, plot_6)
 
 # writing file with cleaned dummified data
 write.csv(train_rel, "train_relevant", row.names=FALSE )
